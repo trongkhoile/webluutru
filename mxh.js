c7f0d74410aa7b4ee7f72018dc4fd3fbed96933a
Ray <80833913+trongkhoile@users.noreply.github.com>
2021-11-17 15:25:50 +0700

    Add files via upload

---
+++ mxh.js
@@ -0,0 +1,171 @@
+let mang = [];
+let mangtimkiem = [];
+window.onload = function () {
+  load();
+  timkiem();
+};
+function bam() {
+  check();
+  chay();
+}
+function check() {
+  var tenmuc = $(".input").val();
+  if (tenmuc == "") {
+    return "a";
+  } else if (localStorage.getItem("mang") === null) {
+    return "c";
+  } else {
+    let mang = localStorage.getItem("mang");
+    mang = JSON.parse(mang);
+
+    mang = mang || [];
+    for (element of mang) {
+      if (tenmuc == element.tenmuc) {
+        return "d";
+      }
+    }
+
+    return "e";
+  }
+}
+
+function chay() {
+  let tenmuc = $(".input").val();
+
+  if (check() == "a") {
+    alert("Bạn chưa nhập tên mục");
+  } else if (check() == "d") {
+    alert("Trùng tên mục");
+  } else if (check() == "c") {
+    var key = Date.now();
+    const html = `
+        <div class="congcu" data-key="${key}"> 
+        <div class="khungnho " >
+        <h2 class="tm">${tenmuc}</h2>
+        <div>
+        <button class="nutbam5 w3-hover-yellow" onclick="xoa()">Xóa</button>
+        </div>
+
+        </div>
+        <br><br>
+        </div>
+        `;
+    const muc = {
+      id: Date.now(),
+      tenmuc: tenmuc,
+      muccon: [],
+    };
+    let mang = localStorage.getItem("mang");
+    mang = JSON.parse(mang);
+    mang = mang || [];
+    mang.push(muc);
+    localStorage.removeItem("mang");
+    localStorage.setItem("mang", JSON.stringify(mang));
+    const timkiem = `<option class="d" value="${tenmuc}">`;
+    $(".khung3").append(html);
+    $("#tencacmuc").append(timkiem);
+  } else if (check() == "e") {
+    var key = Date.now();
+    const html = `
+    <div class="congcu" data-key="${key}"> 
+    <div class="khungnho " >
+    <h2 class="tm">${tenmuc}</h2>
+    <div class="khungdung">
+    
+    <button class="nutbam5 w3-hover-yellow" onclick="xoa()">Xóa</button>
+    </div>
+
+    </div>
+    <br><br>
+    </div>
+    `;
+    const muc = {
+      id: Date.now(),
+      tenmuc: tenmuc,
+      muccon: [],
+    };
+    let mang = localStorage.getItem("mang");
+    mang = JSON.parse(mang);
+    mang.push(muc);
+    localStorage.removeItem("mang");
+    localStorage.setItem("mang", JSON.stringify(mang));
+    const timkiem = `<option class="d" value="${tenmuc}">`;
+    $(".khung3").append(html);
+
+    $("#tencacmuc").append(timkiem);
+  }
+}
+
+//Cho nay day a //////////////////////////////////////
+
+function load() {
+  let mang = localStorage.getItem("mang");
+  mang = JSON.parse(mang);
+  let cacmuc = "";
+  for (e of mang) {
+    cacmuc += `<div class="congcu" data-key:"${e.id}"> 
+    <div class="khungnho " >
+    <h2 class="tm">${e.tenmuc}</h2>
+    <div class="khungdung">
+    
+    <button class="nutbam5 w3-hover-yellow" onclick="xoa()">Xóa</button>
+    </div>
+
+    </div>
+    <br><br>
+    </div>`;
+  }
+  document.querySelector(".khung3").innerHTML = cacmuc;
+}
+function xoa() {
+  $(".congcu").click(function () {
+    let mang = localStorage.getItem("mang");
+    mang = JSON.parse(mang);
+    var giatri = $(this).attr("data-key");
+    for (var i = 0; i < mang.length; i++) {
+      if (Number(mang[i].id) == Number(giatri)) {
+        mang[i] = 0;
+      }
+    }
+
+    localStorage.removeItem("mang");
+    localStorage.setItem("mang", JSON.stringify(mang));
+    checkag();
+    $(this).remove();
+  });
+}
+
+////////////////////////////////////////////
+
+function bam1() {
+  let mang = localStorage.getItem("mang");
+  mang = JSON.parse(mang);
+  var dulieu = $(".input3").val();
+  var input1 = $("input1").val();
+  var input2 = $("input2").val();
+  const h = `<a href="${input2}"><h2>${input1}</h2> </a> `;
+}
+function timkiem() {
+  let mang = localStorage.getItem("mang");
+  mang = JSON.parse(mang);
+  let cacmuctk = "";
+  for (e of mang) {
+    cacmuctk += `<option class="d" value="${e.tenmuc}">`;
+  }
+  $("#tencacmuc").append(cacmuctk);
+}
+
+function checkag() {
+  let mang = localStorage.getItem("mang");
+  mang = JSON.parse(mang);
+  let mangmoi = mang.filter(function (e) {
+    return e != 0;
+  });
+
+  localStorage.removeItem("mang");
+  localStorage.setItem("mang", JSON.stringify(mangmoi));
+}
+function loadptnho() {
+  let mang = localStorage.getItem("mang");
+  mang = JSON.parse(mang);
+}
